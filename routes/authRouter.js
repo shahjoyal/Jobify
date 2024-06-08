136a18fbbd29a6ea64ec013c6342b5dcc9f51a8f
shahjoyal <132144905+shahjoyal@users.noreply.github.com>
2024-06-08 22:03:34 +0530

    Add files via upload

---
+++ routes/authRouter.js
@@ -0,0 +1,30 @@
+// import { Router } from 'express';
+// const router = Router();
+// import { login, logout, register } from '../controllers/authController.js';
+// import {
+//   validateRegisterInput,
+//   validateLoginInput,
+// } from '../middleware/validationMiddleware.js';
+
+// import rateLimiter from 'express-rate-limit';
+
+// const apiLimiter = rateLimiter({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 20,
+//   message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
+// });
+
+// router.post('/register', apiLimiter, validateRegisterInput, register);
+// router.post('/login', apiLimiter, validateLoginInput, login);
+// router.get('/logout', logout);
+
+// export default router;
+import { Router } from 'express';
+import { register, login } from '../controllers/authController.js';
+import { validateRegisterInput } from '../middleware/validationMiddleware.js';
+const router = Router();
+
+router.post('/register',validateRegisterInput, register);
+router.post('/login', login);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,45 @@
+import 'express-async-errors';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app = express();
+import morgan from 'morgan';
+import jobRouter from './routes/jobRouter.js';
+import mongoose from 'mongoose';
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+
+import authRouter from './routes/authRouter.js';
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+  app.use('/api/v1/jobs', jobRouter);
+  app.use('/api/v1/auth', authRouter);
+  app.use('*', (req, res) => {
+    res.status(404).json({ msg: 'not found' });
+  });
+  app.use(errorHandlerMiddleware);
+  const port = process.env.PORT || 5100;
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`server running on PORT ${port}....`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  
+  // app.listen(port, () => {
+  //   console.log(`server running on PORT ${port}....`);
+  // });
